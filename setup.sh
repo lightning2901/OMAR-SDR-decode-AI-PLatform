123d1800321fc80b377924b026a7ac09a87ef59b
lightning2901 <119823416+lightning2901@users.noreply.github.com>
2026-01-28 22:11:11 -0600

    Add files via upload
    Instalación de dependencias para el RTL añadidas.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "~~~~~Setup 1.0 de OMAR~~~~~"
 sudo apt update && sudo apt upgrade -y
 
 #dependencias para el sistema y para el rtl 
-echo "Iniciando instalación de dependencias del sistema mediante apt 1 de 2"
+echo "Iniciando instalación de dependencias del sistema (generales) mediante apt 1 de 2"
 sudo apt install -y \
      git \
      rtl-sdr \
@@ -16,9 +16,19 @@ sudo apt install -y \
      libsox-fmt-all \
      wireless-tools \
      net-tools
-echo "Instalación de bibliotecas finalizada"
+echo "Instalación de bibliotecas generales finalizada"
+echo "Iniciando instalación de dependencias del sistema (RTL-SDR) 2 de 2"
+sudo apt install -y \
+     build-essential \
+     cmake \
+     git \
+     libusb-1.0-0-dev \
+     pkg-config \
+     librtlsdr-dev \
+     rtl-sdr \
+     sox
 # una vez puesta la base, van las librerías de Python
-echo "Iniciando Instalación de Bibliotecas de Python mediante pip 2 de 2"
+echo "Iniciando Instalación de Bibliotecas de Python mediante pip 1 de 1"
 pip install \
     request \
     numpy \
