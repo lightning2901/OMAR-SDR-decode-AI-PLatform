ae4190b78dd5b46991b6e387e8b4c44217672c7d
lightning2901 <119823416+lightning2901@users.noreply.github.com>
2026-02-28 15:12:55 -0600

    Add files via upload

--- instalacion/setup.sh
+++ instalacion/setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+# IMPORTANTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+# CAMBIOS!!!! 28 FEB 2026
+# 1.- EN LA PARTE SUPERIOR VAN TODAS LAS LIBRERÍAS GLOBALES DEL SISTEMA MEDIANTE APT
+# 2.- DEPENDIENDO DEL HARDWARE OBJETIVO, YA SEA ESTACIÓN O SERVIDOR DEBE DE SELECCIONARSE AL INICIO PARA QUE EL INSTALADOR FUNCIONE 
+# 3.- AL IGUAL QUE EL PUNTO ANTERIOR, LOS ENTORNOS VIRTUALES CREADOS RESPONDEN A LA PLATAFORMA ELEGIDA EN EL INSTALADOR AL INICIO
 
-echo "~~~~~Setup 1.0 de OMAR~~~~~"
+echo "~~~~~Setup 1.1 de OMAR~~~~~ \n ESTE INSTALADOR SÓLO DEBE SER UTILIZADO EN EL LINCC PARA LA CREACIÓN DE ESTACIONES O DE UN SERVIDOR OMAR"
 
 sudo apt update && sudo apt upgrade -y
 
 #dependencias para el sistema y para el rtl 
-echo "Iniciando instalación de dependencias del sistema (generales) mediante apt 1 de 2"
+echo "Iniciando instalación de dependencias generales del sistema y para el uso del rtl 1 de 2"
 sudo apt install -y \
      git \
      rtl-sdr \
@@ -16,8 +21,8 @@ sudo apt install -y \
      libsox-fmt-all \
      wireless-tools \
      net-tools
-echo "Instalación de bibliotecas generales finalizada"
-echo "Iniciando instalación de dependencias del sistema (RTL-SDR) 2 de 2"
+echo "Terminado"
+echo "Iniciando instalación de dependencias generales del sistema y para el uso del rtl 2 de 2"
 sudo apt install -y \
      build-essential \
      cmake \
@@ -28,6 +33,9 @@ sudo apt install -y \
      rtl-sdr \
      sox
 # una vez puesta la base, van las librerías de Python
+
+
+
 echo "Iniciando Instalación de Bibliotecas de Python mediante pip 1 de 1"
 pip install \
     request \
